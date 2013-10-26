49930271395e5afc6e537ff006306d14769bc9e4
Randy Lai <randy.cs.lai@gmail.com>
2013-10-25 22:16:19 -0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+Syntax Manager for Sublime Text 2/3
+====================
+Manage settings for muliple syntax
---
+++ syntaxmgr.py
@@ -0,0 +1,18 @@
+import sublime, sublime_plugin
+import os
+
+class TrimListener(sublime_plugin.EventListener):
+    def on_load(self, view):
+        if view.is_scratch() or view.settings().get('is_widget'): return
+        syntaxmgr_settings = view.settings().get("syntaxmgr_settings")
+        if not syntaxmgr_settings: return
+
+        for ss in syntaxmgr_settings:
+            scopes = ss["scopes"] if "scopes" in ss else []
+            extensions = ss["extensions"] if "extensions" in ss else []            
+            fname = view.file_name()
+            if view.score_selector(0, " ".join(scopes)) or fname.lower().endswith(tuple(extensions)):
+                for key, value in ss["settings"].items():
+                    view.settings().set(key, value)
+
+
