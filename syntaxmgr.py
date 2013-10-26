d7880605f8e0a5445c300c05e3f997edcdee8f68
Randy Lai <randy.cs.lai@gmail.com>
2013-10-26 01:05:52 -0700

    getting file name outside the loop

--- syntaxmgr.py
+++ syntaxmgr.py
@@ -5,12 +5,12 @@ class SyntaxMgrListener(sublime_plugin.EventListener):
         if view.is_scratch() or view.settings().get('is_widget'): return
         syntaxmgr_settings = sublime.load_settings('SyntaxMgr.sublime-settings').get("syntaxmgr_settings")
         if not syntaxmgr_settings: return
+        fname = view.file_name()
 
         for ss in syntaxmgr_settings:
             scopes = ss["scopes"] if "scopes" in ss else []
             extensions = ss["extensions"] if "extensions" in ss else []   
             extensions = ["." + e for e in extensions]         
-            fname = view.file_name()
             if (not scopes or view.score_selector(0, " ".join(scopes))) and \
                (not extensions or fname.lower().endswith(tuple(extensions))):
                 for key, value in ss["settings"].items():
