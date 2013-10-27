6bb57f7de49dc4019727de4bba56dff53d68ca7b
Randy Lai <randy.cs.lai@gmail.com>
2013-10-26 22:26:40 -0700

    More OO

--- syntaxmgr.py
+++ syntaxmgr.py
@@ -1,19 +1,34 @@
 import sublime, sublime_plugin
 
+class Sobj():
+    def __init__(self, view, S):
+        self.scopes = S["scopes"] if "scopes" in S else []
+        self.extensions = S["extensions"] if "extensions" in S else []
+        self.settings = S["settings"] if "settings" in S else []
+        self.view =view
+
+    def apply(self):
+        view = self.view
+        for key, value in self.settings.items():
+            view.settings().set(key, value)
+
+    def check(self):
+        view = self.view
+        fname = view.file_name()
+        in_scopes = not self.scopes or any([view.score_selector(0, s)>0 for s in self.scopes])
+        extensions = ["." + e for e in self.extensions]
+        in_extensions = not extensions or fname.lower().endswith(tuple(extensions))
+        return in_scopes and in_extensions
+
+
 class SyntaxMgrListener(sublime_plugin.EventListener):
     def on_load(self, view):
         if view.is_scratch() or view.settings().get('is_widget'): return
-        syntaxmgr_settings = sublime.load_settings('SyntaxMgr.sublime-settings').get("syntaxmgr_settings")
-        if not syntaxmgr_settings: return
-        fname = view.file_name()
+        for S in self.load_settings(view):
+            if S.check(): S.apply()
 
-        for ss in syntaxmgr_settings:
-            scopes = ss["scopes"] if "scopes" in ss else []
-            extensions = ss["extensions"] if "extensions" in ss else []   
-            extensions = ["." + e for e in extensions] 
-            if (not scopes or any([view.score_selector(0, s)>0 for s in scopes])) and \
-               (not extensions or fname.lower().endswith(tuple(extensions))):
-                for key, value in ss["settings"].items():
-                    view.settings().set(key, value)
+    def load_settings(self, view):
+        syntaxmgr_settings = sublime.load_settings('SyntaxMgr.sublime-settings').get("syntaxmgr_settings")
+        return [Sobj(view, S) for S in syntaxmgr_settings]
 
 
