89185f07774f129cdd8bd40a9548267419a3ebc2
Randy Lai <randy.cs.lai@gmail.com>
2013-10-26 18:28:40 -0700

    fix bug for multi scopes

--- syntaxmgr.py
+++ syntaxmgr.py
@@ -10,8 +10,8 @@ class SyntaxMgrListener(sublime_plugin.EventListener):
         for ss in syntaxmgr_settings:
             scopes = ss["scopes"] if "scopes" in ss else []
             extensions = ss["extensions"] if "extensions" in ss else []   
-            extensions = ["." + e for e in extensions]         
-            if (not scopes or view.score_selector(0, " ".join(scopes))) and \
+            extensions = ["." + e for e in extensions] 
+            if (not scopes or any([view.score_selector(0, s)>0 for s in scopes])) and \
                (not extensions or fname.lower().endswith(tuple(extensions))):
                 for key, value in ss["settings"].items():
                     view.settings().set(key, value)
