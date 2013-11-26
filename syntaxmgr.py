6eef1d8e09aa4354df750eaa4fdae24bfc9231ab
Randy Lai <randy.cs.lai@gmail.com>
2013-11-25 19:19:47 -0800

    code tidy up and use IS_LOADED

--- syntaxmgr.py
+++ syntaxmgr.py
@@ -1,20 +1,19 @@
 import sublime, sublime_plugin
 
+IS_LOADED = {}
+
 class Sobj():
-    def __init__(self, view, S):
+    def __init__(self, S):
         self.scopes = S["scopes"] if "scopes" in S else []
         self.scopes_excluded = S["scopes_excluded"] if "scopes_excluded" in S else []
         self.extensions = S["extensions"] if "extensions" in S else []
         self.settings = S["settings"] if "settings" in S else []
-        self.view =view
 
-    def apply(self):
-        view = self.view
+    def apply(self, view):
         for key, value in self.settings.items():
             view.settings().set(key, value)
 
-    def check(self):
-        view = self.view
+    def check(self, view):
         fname = view.file_name()
         in_scopes = not self.scopes or any([view.score_selector(0, s)>0 for s in self.scopes])
         in_scopes_excluded = any([view.score_selector(0, s)>0 for s in self.scopes_excluded])
@@ -24,20 +23,21 @@ class Sobj():
 
 
 class SyntaxMgrListener(sublime_plugin.EventListener):
+    settings = None
+    def __init__(self):
+        self.settings = self.load_settings()
 
     def on_activated(self, view):
         if view.is_scratch() or view.settings().get('is_widget'): return
+        if view.size()==0 and not view.file_name(): return
+        if view.id() not in IS_LOADED:
+            IS_LOADED.update({view.id() : True})
+            for S in self.settings:
+                if S.check(view): S.apply(view)
 
-        for S in self.load_settings(view):
-            if S.check(): S.apply()
-
-    def load_settings(self, view):
+    def load_settings(self):
         syntaxmgr_settings = sublime.load_settings('SyntaxMgr.sublime-settings').get("syntaxmgr_settings")
-        return [Sobj(view, S) for S in syntaxmgr_settings]
+        return [Sobj(S) for S in syntaxmgr_settings]
 
-    # remove IS_LOADED[vid] if file closes
-    def on_close(self, view):
-        vid = view.id()
-        if vid in IS_LOADED: IS_LOADED.pop(vid)
 
 
