bd793a3cc3aacad03cb6d6e3c54c5a37fe9d3a15
Josh Goebel <dreamer3@gmail.com>
2015-03-27 23:24:31 -0400

    refactor for clarity and simplify

--- syntaxmgr.py
+++ syntaxmgr.py
@@ -3,40 +3,49 @@ import sublime_plugin
 import re
 
 
-class Sobj():
+class SyntaxManagerCriteria():
     def __init__(self, S):
         self.S = S
 
     def apply(self, view):
-        settings = self.S["settings"] if "settings" in self.S else []
+        settings = self.S.get("settings", [])
         for key, value in settings.items():
             view.settings().set(key, value)
 
-    def check_scopes(self, view):
-        scopes = self.S["scopes"] if "scopes" in self.S else []
-        scopes_excluded = self.S["scopes_excluded"] if "scopes_excluded" in self.S else []
+    def match_scopes(self, view):
+        scopes = self.S.get("scopes", [])
+        scopes_excluded = self.S.get("scopes_excluded", [])
 
         return (not scopes or any([view.score_selector(0, s) > 0 for s in scopes])) \
             and all([view.score_selector(0, s) == 0 for s in scopes_excluded])
 
-    def check_extensions(self, view):
-        extensions = self.S["extensions"] if "extensions" in self.S else []
+    def match_extensions(self, view):
+        extensions = self.S.get("extensions", [])
+        if not extensions:
+            return True
+
         fname = view.file_name()
         extensions = ["." + e for e in extensions]
-        return not extensions or (fname and fname.lower().endswith(tuple(extensions)))
+        return fname and fname.lower().endswith(tuple(extensions))
 
-    def check_platform(self, view):
-        platforms = self.S["platforms"] if "platforms" in self.S else []
-        return not platforms or sublime.platform() in [p.lower() for p in platforms]
+    def match_platform(self, view):
+        platforms = self.S.get("platforms", [])
+        if not platforms:
+            return True
 
-    def check_firstline(self, view):
-        firstlinepat = self.S["firstline"] if "firstline" in self.S else []
-        return not firstlinepat \
-            or re.match(firstlinepat, view.substr(view.line(view.text_point(0, 0))))
+        return sublime.platform() in [p.lower() for p in platforms]
 
-    def check(self, view):
-        return self.check_scopes(view) and self.check_extensions(view) and \
-            self.check_platform(view) and self.check_firstline(view)
+    def match_firstline(self, view):
+        firstlinepat = self.S.get("firstline", [])
+        if not not firstlinepat:
+            return True
+
+        first_line = view.substr(view.line(view.text_point(0, 0)))
+        return re.match(firstlinepat, first_line)
+
+    def match(self, view):
+        return self.match_scopes(view) and self.match_extensions(view) and \
+            self.match_platform(view) and self.match_firstline(view)
 
 
 class SyntaxMgrListener(sublime_plugin.EventListener):
@@ -64,6 +73,6 @@ class SyntaxMgrReload(sublime_plugin.TextCommand):
         view = self.view
         settings = sublime.load_settings('SyntaxMgr.sublime-settings').get("syntaxmgr_settings", [])
         for s in settings:
-            S = Sobj(s)
-            if S.check(view):
-                S.apply(view)
+            criteria = SyntaxManagerCriteria(s)
+            if criteria.match(view):
+                criteria.apply(view)
